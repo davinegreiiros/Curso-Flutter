f5285e4db3c8bc36f3e26dcf260e5b8ba25d34a2
davinegreiiros <davi.negreiiros36@gmail.com>
2022-08-05 14:07:34 -0300

    notação ponto

---
+++ atividadesFundamentos/fundamentos/notacao_ponto.dart
@@ -0,0 +1,15 @@
+main () {
+  double nota = 6.99.roundToDouble();
+  print(nota);
+
+
+  print("texto".toUpperCase());
+
+  String s1 = "Davi Negreiros";
+  String s2 = s1.substring(0, 8);
+  String s3 = s2.toUpperCase();
+  String s4 = s3.padRight(15, "!");
+  print(s2);
+  print(s3);
+  print(s4);
+}

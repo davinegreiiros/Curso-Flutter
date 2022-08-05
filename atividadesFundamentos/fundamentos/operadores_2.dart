4243d4d567aee2cfc1ea90257ea585e32a220cad
davinegreiiros <davi.negreiiros36@gmail.com>
2022-08-05 16:20:48 -0300

    Operações aula 2

---
+++ atividadesFundamentos/fundamentos/operadores_2.dart
@@ -0,0 +1,24 @@
+main () {
+   //Operadores Aritméticos (binários/infix)
+  double a = 2;
+  a = a + 3;
+  a += 3;
+  a -= 3;
+  a *= 3;
+  a /= 5;
+  a %= 2;
+
+  print(a);
+
+  //Operadores Relacionais (binários/infix) -> O Resultado sempre é BOOL (BOOLEANO)
+  print(3 > 2);
+  print(3 >= 3);
+  print(3 > 4);
+  print(3 >= 3);
+  print(3 <= 3);
+  print(3 != 3);
+  print(3 ==3);
+
+  print(2 + 5 < 3 - 1 && 4 + 7 != 7 - 4);
+
+}

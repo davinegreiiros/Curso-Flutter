a9f7e89d8c8d4cfec5b341c292532f65ae4927ee
davinegreiiros <davi.negreiiros36@gmail.com>
2022-08-10 21:16:55 -0300

    aula de generics

---
+++ atividadesFundamentos/fundamentos/gererics.dart
@@ -0,0 +1,15 @@
+main () {
+
+  List<String> frutas = ['banana', 'maça', 'laranja'];
+  frutas.add('melao');
+  print(frutas);
+
+  Map<String, double> salario = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiario': 600.00,
+  };
+
+  print(salario);
+
+}

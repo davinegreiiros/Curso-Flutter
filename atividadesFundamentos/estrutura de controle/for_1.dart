9451a32386eef9a600405cd34232cd7f2df9e8b7
davinegreiiros <davi.negreiiros36@gmail.com>
2022-08-11 14:23:18 -0300

    aula de for 01

---
+++ atividadesFundamentos/estrutura de controle/for_1.dart
@@ -0,0 +1,22 @@
+main (){
+  
+
+  for(int a = 1; a <= 10 ; a+= 2){
+  print('a = $a');
+  }
+
+  print("FIM!");
+
+
+for(int i = 100; i>= 0 ; i-=4){
+  print('i = $i');
+
+   print("FIM!");
+}
+
+int b = 0;
+for(; b<= 10; b++){
+  print('b = $b');
+}
+  
+}
---
+++ atividadesFundamentos/estrutura de controle/if_else_desafio.dart
@@ -0,0 +1,11 @@
+main () {
+  var nota = 2.3;
+
+  if(nota >= 9)   // ; não usa ; em estrutura de controle * 
+  {
+    print('Parabens! Você foi brilhante!');
+  
+  }
+
+  // *EXCELÇÃO DO/WHIELE!!!
+}

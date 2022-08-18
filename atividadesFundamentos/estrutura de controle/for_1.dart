31c18a4f0c7105e194a95febfec574d11d754584
davinegreiiros <davi.negreiiros36@gmail.com>
2022-08-17 22:54:28 -0300

    for aula 2

--- atividadesFundamentos/estrutura de controle/for_1.dart
+++ atividadesFundamentos/estrutura de controle/for_1.dart
@@ -15,8 +15,14 @@ for(int i = 100; i>= 0 ; i-=4){
 }
 
 int b = 0;
-for(; b<= 10; b++){
-  print('b = $b');
+  for(; b<= 10; b++){
+    print('b = $b');
+  }
+  print('Fim!!');
+
+var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+for(var i = 0; i < notas.length; i++){
+  print("Notas ${i + 1} = ${notas[i]}");
 }
-  
+
 }
---
+++ atividadesFundamentos/estrutura de controle/for_2.dart
@@ -0,0 +1,10 @@
+//for in
+
+main (){
+  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+
+  for(var nota in notas){
+    print("O valor da nota é $nota");
+  }
+
+}
